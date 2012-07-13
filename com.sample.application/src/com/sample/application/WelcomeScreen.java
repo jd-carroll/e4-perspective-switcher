6f6c8221be3b6eb81fa2f9d00f4b73f923defaf4
Joseph Carroll <jdsalingerjr@gmail.com>
2012-07-13 08:26:57 -0500

    Added sample application

---
+++ com.sample.application/src/com/sample/application/WelcomeScreen.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Joseph Carroll and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Joseph Carroll <jdsalingerjr@gmail.com> - initial API and implementation
+ ******************************************************************************/ 
+package com.sample.application;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class WelcomeScreen {
+	
+	
+	@PostConstruct
+	void createControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.None);
+		composite.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_BLUE));
+	}
+	
+	
+	
+	@Focus
+	public void onFocus() {
+		//TODO Your code here
+	}
+	
+	
+}
